9fa7f26d09a6d7f16bad05995382a21e69511796
Maryam Alshaikh <alshaiikh113@gmail.com>
2026-03-15 08:26:31 +0300

    plan page core UI

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 const exploreBtn = document.getElementById("explore-btn");
+const homePage = document.querySelector(".home-page");
+const planPage = document.querySelector(".planning-page");
+const backBtn = document.getElementById("back-btn");
+const homeBtn = document.getElementById("home-btn");
 
-exploreBtn.addEventListener("click", )
+// click on explore
+exploreBtn.addEventListener("click", () => {
+    homePage.classList.add("hidden");
+    planPage.classList.remove("hidden");
+    backBtn.classList.remove("hidden");
+
+    document.body.classList.add('solid-bg')
+})
+
+// click on back
+backBtn.addEventListener("click", () => {
+    homePage.classList.remove("hidden");
+    planPage.classList.add("hidden");
+    backBtn.classList.add("hidden");
+
+    document.body.classList.remove('solid-bg')
+})
